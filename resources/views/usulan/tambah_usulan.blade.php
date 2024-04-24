54377543f42ae2ba289b0fea4a8961286f6d15b2
Rr. Faydiqta R.N.B <diqta230604@gmail.com>
2024-04-25 01:07:22 +0700

    Step 3

--- resources/views/usulan/tambah_usulan.blade.php
+++ resources/views/usulan/tambah_usulan.blade.php
@@ -271,41 +271,29 @@
 
     @if ($data['step'] == '3')
         <!-- step 3 -->
-        <form action="">
-            <div class="row mb-1">
-                <div class="col">
-                    <div class="card card-outline card-primary">
-                        <div class="card-header">
-                            <h4 class="card-title"><b>Capaian</b></h4>
-                        </div>
-                        <div class="card-body">
-                        </div>
-                    </div>
+<form action="">
+    <div class="row mb-1">
+        <div class="col">
+            <div class="card card-outline card-primary">
+                <div class="card-header">
+                    <h4 class="card-title"><b>Capaian</b></h4>
                 </div>
-                <div class="col">
-                    <div class="card card-outline card-primary">
-                        <div class="card-header">
-                            <h4 class="card-title"><b>Berkas Usulan</b></h4>
-                        </div>
-                        <div class="card-body">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="container mb-1">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row d-flex justify-content-between">
-                            <div class="col">
-                                <button type="button" class="btn btn-warning"><b>Kembali</b></button>
+                <div class="card-body">
+                <div class="row form-group-row mt-3">
+                                <div class="col-sm">
+                                    <label>Luaran Tambahan<b class="text-danger">*</b></label>
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="" disabled>
+                                </div>
                             </div>
-                            <div class="col">
-                                <button type="button" class="btn btn-primary"><b>Simpan Draft</b></button>
-                            </div>
-                            <div class="col">
-                                <button type="button" class="btn btn-success"><b>Simpan Permanen</b></button>
-                            </div>
-
+                        <div class="row form-group-row mt-3">
+                                <div class="col-sm">
+                                    <label>IKU<b class="text-danger">*</b></label>
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="" disabled>
+                                </div>
                         </div>
                 </div>
             </div>
@@ -327,6 +315,25 @@
                     </div>
                 </div>
             </div>
+        </div>
+
+<div class="container mb-1">
+        <div class="card">
+            <div class="card-body">
+                <div class="row d-flex justify-content-between">
+                    <div class="col">
+                        <button type="button" class="btn btn-warning"><b>Kembali</b></button>
+                    </div>
+                    <div class="col">
+                        <button type="button" class="btn btn-primary"><b>Simpan Draft</b></button>
+                    </div>
+                    <div class="col">
+                        <button type="button" class="btn btn-success"><b>Simpan Permanen</b></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
         </form>
     @endif
 @endsection
